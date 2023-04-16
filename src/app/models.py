fb9fc9d2fc02b53288bab89607df4b239dc4d984
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-04-16 21:24:25 +0300

    migrations

--- src/app/models.py
+++ src/app/models.py
@@ -4,5 +4,13 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(
+        max_length=100, verbose_name='Name of the settlement')
     slug = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+        verbose_name = 'Name of the settlement'
+        verbose_name_plural = 'Name of settlements'
+
+    def __str__(self):
+        return self.name
