c8e3bc5ab73728b9bfe8f4717d629ba80e5930c9
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-04-17 20:38:25 +0300

    function for job mappings

--- src/app/views.py
+++ src/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from app.models import Job
 
 # Create your views here.
+
+
+def home_view(request):
+    qs = Job.objects.all()
+    return render(request, 'home.html', {'object_list': qs})
