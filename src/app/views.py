c79759df79ce2cbb80f84c5690549f5205a438b8
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-05-07 15:24:25 +0300

    Search by parameters from the form

--- src/app/views.py
+++ src/app/views.py
@@ -5,5 +5,17 @@ from app.models import Job
 
 
 def home_view(request):
-    qs = Job.objects.all()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__name'] = city
+        if language:
+            _filter['language__name'] = language
+
+        qs = Job.objects.filter(**_filter)
+
     return render(request, 'scraping/home.html', {'object_list': qs})
