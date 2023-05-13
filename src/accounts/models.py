611867ec462ff3b8382e19eb745e130f77cdff13
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-05-13 16:37:47 +0300

    Custom Model Manager

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,13 +1,42 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import UserManager
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    '''
+    Custom user manager for the MyUser model.
+    This manager provides the ability to create regular users and superusers.
+    '''
+
+    def create_user(self, email, password=None):
+
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 
 class MyUser(AbstractBaseUser):
     '''
-    Custom user model
+    Custom user model representing a user of the job board website.
+    The user is identified by their email address, and can be associated
+    with a city and a programming language.
+    They can also choose to receive email notifications about job postings.
     '''
     email = models.EmailField(
         verbose_name='Email address',
@@ -18,13 +47,13 @@ class MyUser(AbstractBaseUser):
     is_admin = models.BooleanField(default=False)
 
     city = models.ForeignKey(
-        'City',
+        'app.City',
         on_delete=models.SET_NULL,
         null=True,
         blank=True)
 
     language = models.ForeignKey(
-        'Language',
+        'app.Language',
         on_delete=models.SET_NULL,
         null=True,
         blank=True
@@ -32,7 +61,7 @@ class MyUser(AbstractBaseUser):
 
     send_email = models.BooleanField(default=False)
 
-    objects = UserManager()
+    objects = MyUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
