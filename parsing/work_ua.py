880006bdadc8c5fd5b18014224fc559c07d5cd53
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-05-14 17:26:23 +0300

    Division into functions. Parsing dou.ua

--- parsing/work_ua.py
+++ parsing/work_ua.py
@@ -1,5 +1,4 @@
 import requests
-# import codecs
 import json
 from bs4 import BeautifulSoup as bs
 
@@ -8,58 +7,106 @@ headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
 }
 
-domain = 'https://www.work.ua'
-url = 'https://www.work.ua/jobs-kyiv-python/'
 
-resp = requests.get(url, headers=headers)
+def work_ua(url):
 
-jobs = []
+    domain = 'https://www.work.ua'
+    resp = requests.get(url, headers=headers)
 
-errors = []
+    jobs = []
+    errors = []
 
-if resp.status_code == 200:
-    soup = bs(resp.content, 'html.parser')
-    main_div = soup.find('div', id='pjax-job-list')
+    if resp.status_code == 200:
+        soup = bs(resp.content, 'html.parser')
+        main_div = soup.find('div', id='pjax-job-list')
 
-    if main_div:
-        div_list = main_div.find_all(
-            'div', attrs={
-                'class': 'job-link'})
+        if main_div:
+            div_list = main_div.find_all(
+                'div', attrs={
+                    'class': 'job-link'})
 
-        for div in div_list:
-            title = div.find('h2')
-            href = title.a['href']
-            content = div.p.text
-            company = 'No name'
-            logo = div.find('img')
+            for div in div_list:
+                title = div.find('h2')
+                href = title.a['href']
+                content = div.p.text
+                company = 'No name'
+                logo = div.find('img')
 
-            if logo:
-                company = logo['alt']
+                if logo:
+                    company = logo['alt']
 
-            jobs.append({
-                'title': title.text,
-                'href': domain + href,
-                'description': content,
-                'company': company
+                jobs.append({
+                    'title': title.text,
+                    'href': domain + href,
+                    'description': content,
+                    'company': company
+                })
+
+        else:
+            errors.append({
+                'url': url,
+                'title': "<div> doesn't exist",
             })
 
     else:
         errors.append({
             'url': url,
-            'title': "<div> doesn't exist",
+            'title': "Page doesn't respond",
         })
 
-else:
-    errors.append({
-        'url': url,
-        'title': "Page doesn't respond",
-    })
+    json_str = json.dumps(jobs, ensure_ascii=False, indent=4)
 
-json_str = json.dumps(jobs, ensure_ascii=False, indent=4)
+    with open('parsing/web/work_ua.json', 'w', encoding='utf-8') as f:
+        f.write(json_str)
 
-with open('parsing/web/work_ua.json', 'w', encoding='utf-8') as f:
-    f.write(json_str)
 
-# h = codecs.open('parsing/web/work_ua.json', 'w', encoding='utf-8')
-# h.write(str(jobs))
-# h.close()
+def dou_ua(url):
+    resp = requests.get(url, headers=headers)
+
+    jobs = []
+    errors = []
+
+    if resp.status_code == 200:
+        soup = bs(resp.content, 'html.parser')
+        main_div = soup.find('div', id='vacancyListId')
+
+        if main_div:
+            li_list = main_div.find_all('li', attrs={'class': 'l-vacancy'})
+
+            for li in li_list:
+                title_div = li.find('div', {'class': 'title'})
+                title_a = title_div.find('a', {'class': 'vt'})
+                title = title_a.text.strip()
+                href = title_a['href']
+                company_a = title_div.find('a', {'class': 'company'})
+                company = company_a.text.split('\xa0')[-1]
+                content_div = li.find('div', {'class': 'sh-info'})
+                content = content_div.text.strip()
+
+                jobs.append({
+                    'title': title,
+                    'href': href,
+                    'description': content,
+                    'company': company
+                })
+
+        else:
+            errors.append({
+                'url': url,
+                'title': "<div> doesn't exist",
+            })
+
+    else:
+        errors.append({
+            'url': url,
+            'title': "Page doesn't respond",
+        })
+
+    json_str = json.dumps(jobs, ensure_ascii=False, indent=4)
+
+    with open('parsing/web/dou_ua.json', 'w', encoding='utf-8') as f:
+        f.write(json_str)
+
+
+# work_ua('https://www.work.ua/jobs-kyiv-python/')
+dou_ua('https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D1%97%D0%B2&category=Python')
