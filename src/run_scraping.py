80946b5cd507d3052f3711ee13e26cdc88cf4cac
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-05-15 18:26:01 +0300

    Running Django outside the project itself

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -1,14 +1,28 @@
 import codecs
 import json
+import os
+import sys
+import django
 from app.parsers import work_ua, dou_ua, djinni_co
+from app.models import Language, City, Job
 
-jobs, errors = [], []
+
+proj = os.path.dirname(os.path.abspath('manage.py'))
+
+sys.path.append(proj)
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
+django.setup()
+
 
 parsers = (
     (work_ua, 'https://www.work.ua/jobs-kyiv-python/'),
     (dou_ua, 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D1%97%D0%B2&category=Python'),
     (djinni_co, 'https://djinni.co/jobs/?primary_keyword=Python&region=UKR&location=kyiv')
 )
+
+city = City.objects.filter(slug='kiev')
+jobs, errors = [], []
 
 for func, url in parsers:
     j, e = func(url)
