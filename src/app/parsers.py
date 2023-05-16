2cc3386b5ec84091927740e2547e41f95dc81bda
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-05-16 15:00:25 +0300

    During the development phase and save the received jobs in the database

--- src/app/parsers.py
+++ src/app/parsers.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup as bs
 from random import randint
 
+__all__ = ('work_ua', 'dou_ua', 'djinni_co')
+
 headers = [
     {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
@@ -18,152 +20,163 @@ headers = [
 ]
 
 
-def work_ua(url):
-
-    domain = 'https://www.work.ua'
-    resp = requests.get(url, headers=headers[randint(0, 2)])
+def work_ua(url, city=None, language=None):
 
     jobs = []
     errors = []
+    domain = 'https://www.work.ua'
 
-    if resp.status_code == 200:
-        soup = bs(resp.content, 'html.parser')
-        main_div = soup.find('div', id='pjax-job-list')
+    if url:
+        resp = requests.get(url, headers=headers[randint(0, 2)])
 
-        if main_div:
-            div_list = main_div.find_all(
-                'div', attrs={
-                    'class': 'job-link'})
+        if resp.status_code == 200:
+            soup = bs(resp.content, 'html.parser')
+            main_div = soup.find('div', id='pjax-job-list')
 
-            for div in div_list:
-                title = div.find('h2')
-                href = title.a['href']
-                content = div.p.text
-                company = 'No name'
-                logo = div.find('img')
+            if main_div:
+                div_list = main_div.find_all(
+                    'div', attrs={
+                        'class': 'job-link'})
 
-                if logo:
-                    company = logo['alt']
+                for div in div_list:
+                    title = div.find('h2')
+                    href = title.a['href']
+                    content = div.p.text
+                    company = 'No name'
+                    logo = div.find('img')
 
-                jobs.append({
-                    'title': title.text,
-                    'href': domain + href,
-                    'description': content,
-                    'company': company
+                    if logo:
+                        company = logo['alt']
+
+                    jobs.append({
+                        'title': title.text,
+                        'url': domain + href,
+                        'description': content,
+                        'company': company,
+                        'city_id': city,
+                        'language_id': language
+                    })
+
+            else:
+                errors.append({
+                    'url': url,
+                    'title': "<div> doesn't exist",
                 })
 
         else:
             errors.append({
                 'url': url,
-                'title': "<div> doesn't exist",
+                'title': "Page doesn't respond",
             })
-
-    else:
-        errors.append({
-            'url': url,
-            'title': "Page doesn't respond",
-        })
 
     return jobs, errors
 
 
-def dou_ua(url):
-    resp = requests.get(url, headers=headers[randint(0, 2)])
+def dou_ua(url, city=None, language=None):
 
     jobs = []
     errors = []
+    # domain = 'https://www.work.ua'
 
-    if resp.status_code == 200:
-        soup = bs(resp.content, 'html.parser')
-        main_div = soup.find('div', id='vacancyListId')
+    if url:
+        resp = requests.get(url, headers=headers[randint(0, 2)])
 
-        if main_div:
-            li_list = main_div.find_all('li', attrs={'class': 'l-vacancy'})
+        if resp.status_code == 200:
+            soup = bs(resp.content, 'html.parser')
+            main_div = soup.find('div', id='vacancyListId')
 
-            for li in li_list:
-                if '__hot' not in li['class']:
-                    title_div = li.find('div', {'class': 'title'})
-                    title_a = title_div.find('a', {'class': 'vt'})
-                    title = title_a.text.strip()
+            if main_div:
+                li_list = main_div.find_all('li', attrs={'class': 'l-vacancy'})
+
+                for li in li_list:
+                    if '__hot' not in li['class']:
+                        title_div = li.find('div', {'class': 'title'})
+                        title_a = title_div.find('a', {'class': 'vt'})
+                        title = title_a.text.strip()
+                        href = title_a['href']
+                        company_a = title_div.find('a', {'class': 'company'})
+                        company = company_a.text.split('\xa0')[-1]
+                        content_div = li.find('div', {'class': 'sh-info'})
+                        content = content_div.text.strip()
+
+                        jobs.append({
+                            'title': title,
+                            'url': href,
+                            'description': content,
+                            'company': company,
+                            'city_id': city,
+                            'language_id': language
+                        })
+
+            else:
+                errors.append({
+                    'url': url,
+                    'title': "<div> doesn't exist",
+                })
+
+        else:
+            errors.append({
+                'url': url,
+                'title': "Page doesn't respond",
+            })
+
+    return jobs, errors
+
+
+def djinni_co(url, city=None, language=None):
+
+    jobs = []
+    errors = []
+    domain = 'https://djinni.co'
+
+    if url:
+        resp = requests.get(url, headers=headers[randint(0, 2)])
+
+        if resp.status_code == 200:
+            soup = bs(resp.content, 'html.parser')
+            main_ul = soup.find(
+                'ul', attrs={'class': 'list-unstyled list-jobs'})
+
+            if main_ul:
+                li_list = main_ul.find_all(
+                    'li', attrs={'class': 'list-jobs__item list__item'})
+
+                for li in li_list:
+                    title_a = li.find('a', attrs={'class': 'profile'})
+                    title = title_a.span.text.strip()
                     href = title_a['href']
-                    company_a = title_div.find('a', {'class': 'company'})
-                    company = company_a.text.split('\xa0')[-1]
-                    content_div = li.find('div', {'class': 'sh-info'})
-                    content = content_div.text.strip()
+
+                    company_a = li.find(
+                        'div', attrs={'class': 'list-jobs__details__info'})
+                    company = company_a.a.text.strip() if company_a else 'No name'
+
+                    content_div = li.find(
+                        'div', attrs={
+                            'class': 'list-jobs__description position-relative'})
+                    content = content_div.text.strip() if content_div else ''
 
                     jobs.append({
                         'title': title,
-                        'href': href,
+                        'url': domain + href,
                         'description': content,
-                        'company': company
-                    })
+                        'company': company,
+                        'city_id': city,
+                        'language_id': language})
 
-        else:
-            errors.append({
-                'url': url,
-                'title': "<div> doesn't exist",
-            })
-
-    else:
-        errors.append({
-            'url': url,
-            'title': "Page doesn't respond",
-        })
-
-    return jobs, errors
-
-
-def djinni_co(url):
-    resp = requests.get(url, headers=headers[randint(0, 2)])
-
-    jobs = []
-    errors = []
-
-    if resp.status_code == 200:
-        soup = bs(resp.content, 'html.parser')
-        main_ul = soup.find('ul', attrs={'class': 'list-unstyled list-jobs'})
-
-        if main_ul:
-            li_list = main_ul.find_all(
-                'li', attrs={'class': 'list-jobs__item list__item'})
-
-            for li in li_list:
-                title_a = li.find('a', attrs={'class': 'profile'})
-                title = title_a.span.text.strip()
-                href = title_a['href']
-
-                company_a = li.find(
-                    'div', attrs={'class': 'list-jobs__details__info'})
-                company = company_a.a.text.strip() if company_a else 'No name'
-
-                content_div = li.find(
-                    'div', attrs={
-                        'class': 'list-jobs__description position-relative'})
-                content = content_div.text.strip() if content_div else ''
-
-                jobs.append({
-                    'title': title,
-                    'href': href,
-                    'description': content,
-                    'company': company
+            else:
+                errors.append({
+                    'url': url,
+                    'title': "<ul> doesn't exist",
                 })
 
         else:
             errors.append({
                 'url': url,
-                'title': "<ul> doesn't exist",
+                'title': "Page doesn't respond",
             })
-
-    else:
-        errors.append({
-            'url': url,
-            'title': "Page doesn't respond",
-        })
 
     return jobs, errors
 
-
-__all__ = ('work_ua', 'dou_ua', 'djinni_co')
 
 # work_ua('https://www.work.ua/jobs-kyiv-python/')
 # dou_ua('https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D1%97%D0%B2&category=Python')
--- src/run_scraping.py
+++ src/run_scraping.py
@@ -2,34 +2,47 @@ import codecs
 import json
 import os
 import sys
-import django
-from app.parsers import work_ua, dou_ua, djinni_co
-from app.models import Language, City, Job
+from django.db import DatabaseError
 
-
+# Configuring the Django environment
 proj = os.path.dirname(os.path.abspath('manage.py'))
-
 sys.path.append(proj)
-
 os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
+
+import django
 django.setup()
 
+from app.parsers import work_ua, dou_ua, djinni_co
+from app.models import City, Language, Job
 
+# Define the parsers and their respective URLs
 parsers = (
     (work_ua, 'https://www.work.ua/jobs-kyiv-python/'),
     (dou_ua, 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D1%97%D0%B2&category=Python'),
     (djinni_co, 'https://djinni.co/jobs/?primary_keyword=Python&region=UKR&location=kyiv')
 )
 
-city = City.objects.filter(slug='kiev')
+# Extract "City" and "Language" objects
+city = City.objects.filter(slug='kiev').first()
+language = Language.objects.filter(slug='python').first()
+
+# Perform scraping and collect jobs and errors
 jobs, errors = [], []
 
 for func, url in parsers:
-    j, e = func(url)
+    j, e = func(url, city=city, language=language)
     jobs += j
     errors += e
 
-json_str = json.dumps(jobs, ensure_ascii=False, indent=4)
+# Save jobs to the db
+for job in jobs:
+    j = Job(**job)
+    try:
+        j.save()
+    except DatabaseError:
+        pass
 
+# ? Write jobs to JSON file
+json_str = json.dumps(jobs, ensure_ascii=False, indent=4)
 with codecs.open('src/app/parsing_results/all_jobs.json', 'w', encoding='utf-8') as file:
     file.write(json_str)
