9ad53075ab65ac13768ab433d4f7c4d9231b0945
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-05-17 15:08:59 +0300

    Complete editing and redoing the file

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -1,48 +1,58 @@
-import codecs
-import json
 import os
-import sys
-from django.db import DatabaseError
-
-# Configuring the Django environment
-proj = os.path.dirname(os.path.abspath('manage.py'))
-sys.path.append(proj)
-os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
-
 import django
+
+# Set the DJANGO_SETTINGS_MODULE environment variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scraping_service.settings')
+
+# Configure Django settings
 django.setup()
 
+# Import the necessary modules and functions
 from app.parsers import work_ua, dou_ua, djinni_co
 from app.models import City, Language, Job
 
-# Define the parsers and their respective URLs
-parsers = (
-    (work_ua, 'https://www.work.ua/jobs-kyiv-python/'),
-    (dou_ua, 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D1%97%D0%B2&category=Python'),
-    (djinni_co, 'https://djinni.co/jobs/?primary_keyword=Python&region=UKR&location=kyiv')
-)
 
-# Extract "City" and "Language" objects
-city = City.objects.filter(slug='kiev').first()
-language = Language.objects.filter(slug='python').first()
+def run_scraping():
+    # Define the URLs for scraping
+    work_ua_url = 'https://www.work.ua/jobs-kyiv-python/'
+    dou_ua_url = 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D1%97%D0%B2&category=Python'
+    djinni_co_url = 'https://djinni.co/jobs/?primary_keyword=Python&region=UKR&location=kyiv'
 
-# Perform scraping and collect jobs and errors
-jobs, errors = [], []
+    # Scrape data from work.ua
+    work_ua_jobs, work_ua_errors = work_ua(
+        work_ua_url, city=None, language=None)
+    save_jobs(work_ua_jobs)
 
-for func, url in parsers:
-    j, e = func(url)
-    jobs += j
-    errors += e
+    # Scrape data from dou.ua
+    dou_ua_jobs, dou_ua_errors = dou_ua(dou_ua_url, city=None, language=None)
+    save_jobs(dou_ua_jobs)
 
-# Save jobs to the db
-for job in jobs:
-    j = Job(**job, city=city, language=language)
-    try:
-        j.save()
-    except DatabaseError:
-        pass
+    # Scrape data from djinni.co
+    djinni_co_jobs, djinni_co_errors = djinni_co(
+        djinni_co_url, city=None, language=None)
+    save_jobs(djinni_co_jobs)
 
-# ? Write jobs to JSON file
-json_str = json.dumps(jobs, ensure_ascii=False, indent=4)
-with codecs.open('src/app/parsing_results/all_jobs.json', 'w', encoding='utf-8') as file:
-    file.write(json_str)
+
+def save_jobs(jobs):
+    # Iterate over the scraped jobs and save them in the database
+    for job_data in jobs:
+
+        # Get or create the City object
+        city = City.objects.filter(slug='kiev').first()
+
+        # Get or create the Language object
+        language = Language.objects.filter(slug='python').first()
+
+        # Create the Job object and save it
+        Job.objects.create(
+            url=job_data['url'],
+            title=job_data['title'],
+            company=job_data['company'],
+            description=job_data['description'],
+            city=city,
+            language=language
+        )
+
+
+# Run the scraping and database storage
+run_scraping()
