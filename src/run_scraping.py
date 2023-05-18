9c2f23ebcdd0dd193afc6f5b31d610c80f96c12a
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-05-18 18:16:48 +0300

    Obtaining a set of urls, according to the data from users

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -9,7 +9,7 @@ django.setup()
 
 # Import the necessary modules and functions
 from app.parsers import work_ua, dou_ua, djinni_co
-from app.models import City, Language, Job, Error
+from app.models import City, Language, Job, Error, Url
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -26,6 +26,25 @@ def get_settings():
     settings_lst = set((q['city_id'], q['language_id']) for q in qs)
 
     return settings_lst
+
+
+def get_urls(_settings):
+    qs = Url.objects.all().values()
+    url_dct = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
+    urls = []
+
+    for pair in _settings:
+        tmp = {}
+        tmp['city'] = pair[0]
+        tmp['language'] = pair[1]
+        tmp['url_data'] = url_dct[pair]
+        urls.append(tmp)
+
+    return urls
+
+
+q = get_settings()
+u = get_urls(q)
 
 
 def run_scraping():
