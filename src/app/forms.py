fba9eb66ae29caf060d19dbf26b057a68b0ac5f6
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-05-24 18:35:24 +0300

    forms

--- src/app/forms.py
+++ src/app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from app.models import City, Language
+from app.models import City, Language, Job
 
 
 class FindForm(forms.Form):
@@ -17,3 +17,29 @@ class FindForm(forms.Form):
         required=False,
         widget=forms.Select(attrs={'class': 'form-control'}),
         label='Programming Language')
+
+
+class VForm(forms.ModelForm):
+    city = forms.ModelChoiceField(
+        queryset=City.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='City'
+    )
+    language = forms.ModelChoiceField(
+        queryset=Language.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='Specialty'
+    )
+    url = forms.CharField(label='URL', widget=forms.URLInput(
+        attrs={'class': 'form-control'}))
+    title = forms.CharField(label='Job Title', widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    company = forms.CharField(label='The company', widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    description = forms.CharField(
+        label='Job Description',
+        widget=forms.Textarea(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = Job
+        fields = '__all__'
